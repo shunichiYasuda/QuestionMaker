3c3e7cbfb0fc6b3a248bb66fae573d0d60d57dd9
yasuda <shunichi.sd@gmail.com>
2019-08-26 16:56:54 +0900

    edited on CF-SV

--- QuestionMaker/src/application/SampleController.java
+++ QuestionMaker/src/application/SampleController.java
@@ -1,5 +1,28 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class SampleController {
+	@FXML
+	TextArea editPanel;
+	@FXML 
+	TextArea outputPanel;
+	@FXML
+	private void quitAction() {
+		System.exit(0);
+	}
+	@FXML
+	private void openAction() {
+		
+	}
+	@FXML
+	private void saveAction() {
+		
+	}
+	@FXML
+	private void execAction() {
+		
+	}
 	
 }
